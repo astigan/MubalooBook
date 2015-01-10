ac1853189d43362747f0269153cdfc08c0b87301
jamielynch <jamie.lynch01@gmail.com>
2015-01-10 14:54:13 +0000

    initial commit

---
+++ app/src/androidTest/java/com/example/mubaloobook/SanityTest.java
@@ -0,0 +1,14 @@
+package com.example.mubaloobook;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * A test that should always fail, helpful for verifying the project configuration is correct
+ */
+public class SanityTest extends InstrumentationTestCase {
+
+    public void testSanity() {
+        assertEquals(true, false);
+    }
+
+}
